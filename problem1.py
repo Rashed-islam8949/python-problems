62881fff2cd3d7670f0ee7adecc4b8e702861830
Rashed-islam8949 <rashed318949@gmail.com>
2025-09-29 01:55:02 +0600

    solve: add problem1.py

---
+++ problem1.py
@@ -0,0 +1,5 @@
+# QUS 1:Write a PYTHON programe to perform input or output of all bssic data types.
+ch=input("Enter a character:")
+vl=input("Enter a signed short value:")
+print(f"You entered character: '{ch}'")
+print(f"You entered signed short: {vl}")
